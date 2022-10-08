25f2acd78be93aa564d371de04072d15f6822a9d
Lukasz <lukasz.birski@gmail.com>
2022-10-08 16:07:01 +0200

    Small corrections in UI, added binding

--- app/src/main/java/pl/birski/digitrecognition/MainActivity.kt
+++ app/src/main/java/pl/birski/digitrecognition/MainActivity.kt
@@ -1,11 +1,32 @@
 package pl.birski.digitrecognition
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import pl.birski.digitrecognition.databinding.ActivityMainBinding
+import java.io.IOException
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    private lateinit var classifier: Classifier
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        try {
+            classifier = Classifier(this)
+        } catch (e: IOException) {
+            Toast.makeText(this, "Error while using classifier", Toast.LENGTH_LONG).show()
+        }
+
+        binding.btnDetect.setOnClickListener {
+        }
+
+        binding.btnClear.setOnClickListener {
+        }
     }
 }
